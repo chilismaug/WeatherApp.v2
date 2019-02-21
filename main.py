04c130082cacb34f359cd80a7bb61f52d072b74f
charles ross <charles.ross.oh@gmail.com>
2019-02-20 20:15:43 -0500

    tryin crazee sheeit to get ahold of the bucket blob for some json

--- main.py
+++ main.py
@@ -11,24 +11,34 @@ app = Flask(__name__)
 
 # Configure this environment variable via app.yaml
 CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+CLOUD_STORAGE_FOLDER = "data"
 
 @app.route('/')
 def index():
-    filename = 'cities.json'
+    file_name = "cities.json"
 
 #    jsonFile = os.path.join( "static", "data", filename)
 #    jsonFile = os.path.join(os.path.split(__file__)[0], 'data/cities.json')
 
     # create Goo Cloud Storage client to get that json from
-    gcs = storage.client
+    gcs = storage.Client()
     # Get the bucket that the file will be uploaded to.
     bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    list_blobs(CLOUD_STORAGE_BUCKET)
+
     # the data file will be known as a blob to us henceforth
-    blob = bucket.get_blob('json_data/data/cities.json')
-    
+    myblob = bucket.get_blob(CLOUD_STORAGE_FOLDER + "/"+ file_name)
+
+    print("we have blob!")
+    print(myblob.name)
+
     #    goo_cloud_dta_url = 'https://00e9e64bacfaa9cc0617d1a99652a0c37ad7874916782379f3-apidata.googleusercontent.com/download/storage/v1/b/weatherapiflask.appspot.com/o/jsondata%2Fdata%2Fcities.json?qk=AD5uMEvaEIv5MYeC8_uqRLsL9fcjsDsKxIWd_4wkv60NOa1m76ZfK7j3d-h48iUdt7KqSmoMs7pHLwdW0D1_ot60se5bXHssZV44pNlblbXcII6zhapVCN0FEOoMtWhNS97SjmdklBya7DSEToHyL0yiJDjVM7k_q3tdMKjVwZStCLYYhRyxDKMKXu8ovykN6R5iwrtU4h3RcSjrGUThMh0DRkl13fxjDcz4PnTaGXpsJGdIpFDkscAC2icAoowy41abXeAOlwBojjOhYasR2hm7Lun8Ih0v5beo5USJdoxL3Hrpo45al8AOfZVbHjO1s5kiLJftqCLeU8iqerZu9nzj5P0BQs5ZjklXSB83z_5RD7va3btw8kOns3TMGdrsSvKFAsNlMSSg921jTNUlgCtA4o46hK1J9YCOxH0eO9-3uIDTq-ROxB9ySHOS1-tsE86iG2kzeAecaPJrtxumlU21c_B7SVfwFsbI2UaF227sz-Qyj7_QSthHMHDRkwx4l_E5lFFwR7IKlgfiUnm7A5KSpdVzsgLovYnBhCP4Yl1nzVIvzGgrzK3CNLCq-2r4oOIYzKwfCJR9SC8b9oKEthOUNd1ilIHc_fPRzSnVbiWItyiATXZTH8xjhrX776s44BUgE59YoucJdnS8CUlGb_zlAOBf7_LV2QWDAzfaZpqM77gBHw3DDtmm8-8OoawabhyeuVDNKxkGNmW_h_AS9gOmSCphOwaXOCsSdiTJYMMVhHov0dr8vLHtZ0iYxdEaL5GiUDGEJ-jl5UFJjAIETQR3z9cx52ToKArWE0iIA1j1kVq0X_c4PGs'
-    jsonFile =  blob  # feb 19, 2019 : amazed if this works, just sayin'
-    
+
+    jsonFile =   myblob.download_as_string().decode()
+
+    # blob.download_as_string  # feb 19, 2019 : amazed if this works, just sayin'
+
 
     here_data = [{'name':'Canberra'},
                {'name':'Kathmandu'},
@@ -83,6 +93,44 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    print("listing bucket blobs")
+    gcs = storage.Client()
+    bucket = gcs.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        print(blob.name)
+
+def _read_file(blob, format):
+        """Reads a non-notebook file.
+
+        blob: instance of :class:`google.cloud.storage.Blob`.
+        format:
+          If "text", the contents will be decoded as UTF-8.
+          If "base64", the raw bytes contents will be encoded as base64.
+          If not specified, try to decode as UTF-8, and fall back to base64
+        """
+        bcontent = blob.download_as_string()
+
+        if format is None or format == "text":
+            # Try to interpret as unicode if format is unknown or if unicode
+            # was explicitly requested.
+            try:
+                return bcontent.decode("utf8"), "text"
+            except UnicodeError:
+                if format == "text":
+                    raise web.HTTPError(
+                        400, "%s is not UTF-8 encoded" %
+                             blob.file_name,
+                        reason="bad format",
+                    )
+        return base64.encodebytes(bcontent).decode("ascii"), "base64"
+
 
 if __name__=='__main__':
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -2,6 +2,12 @@ from datetime import datetime
 import os
 import pytz
 import requests
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+
 import math
 API_KEY = '40d0c5223044f01a0f0142846dc203d3'
 API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
